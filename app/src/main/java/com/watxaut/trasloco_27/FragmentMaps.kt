e6dee6b632bd73cc3b75a31f895186e2d3c0d66a
Joan Heredia Rius <watxaut@gmail.com>
2019-01-25 13:30:11 +0100

    remove key from repo

--- app/src/main/java/com/watxaut/trasloco_27/FragmentMaps.kt
+++ app/src/main/java/com/watxaut/trasloco_27/FragmentMaps.kt
@@ -4,15 +4,23 @@ import android.content.Context
 import android.os.Bundle
 import android.util.AttributeSet
 import android.widget.Toast
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
-class FragmentMaps : SupportMapFragment(), OnMapReadyCallback {
+class FragmentMaps : SupportMapFragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+
+    override fun onMarkerClick(p0: Marker?) = false
+
     private lateinit var mMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
 
     var initial_latitude  = -34.0
     var initial_longitude = 151.0
@@ -24,6 +32,8 @@ class FragmentMaps : SupportMapFragment(), OnMapReadyCallback {
 
         val typed_array = context.obtainStyledAttributes(attrs,
             R.styleable.MyyMap, 0, 0)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
 
         try {
             val provided_latitude =
@@ -48,11 +58,15 @@ class FragmentMaps : SupportMapFragment(), OnMapReadyCallback {
     }
     override fun onMapReady(map: GoogleMap?) {
         System.err.println("OnMapReady start")
-        mMap = map as GoogleMap;
+        mMap = map as GoogleMap
 
-        val sydney = LatLng(initial_latitude, initial_longitude);
-        mMap.addMarker(MarkerOptions().position(sydney).title(initial_marker));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        val sydney = LatLng(initial_latitude, initial_longitude)
+        mMap.addMarker(MarkerOptions().position(sydney).title(initial_marker))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        map.uiSettings.isZoomControlsEnabled = true
+        map.setOnMarkerClickListener(this)
+
         Toast.makeText(this.context, "OnMapReady end", Toast.LENGTH_LONG).show()
     }
 
